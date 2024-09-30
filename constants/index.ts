1eaf70489c79c06c1b7d376704a713c29d8d8aa3
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-09-30 18:41:30 +0300

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+const artists = [
+  {
+    name: 'arthur paux',
+    path: '/arthur-paux',
+  },
+  {
+    name: 'gabriel porier',
+    path: '/gabriel-porier',
+  },
+  {
+    name: 'kevin le dortz',
+    path: '/kevin-le-dortz',
+  },
+  {
+    name: 'mathieu caplanne',
+    path: '/mathieu-caplanne',
+  },
+  {
+    name: 'romain loiseau',
+    path: '/romain-loiseau',
+  },
+  {
+    name: 'thomas canu',
+    path: '/thomas-canu',
+  },
+];
