37fe173fcc1fb8dee96a1f9bee59fa138164d93b
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-10-03 02:57:57 +0300

    adding db connection and models

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -27,13 +27,13 @@ const Header = () => {
   }
 
   return (
-    <nav className="w-full  p-4 md:p-12 xl:p-16 flex justify-between items-center gap-4">
+    <nav className="w-full  p-8 md:p-12 xl:p-16 flex justify-between items-center gap-4 mb-36">
       <button
         className="relative w-10 h-10 mx-4 flex items-center justify-center group"
         onClick={() => openModal()}
       >
-        <span className="absolute w-2 h-16 bg-gradient-to-t from-[#FCBB53] dark:from-[#FE9492] via-[#FB65A4]   to-[#A67DD2] dark:to-[#0D0DA3] rounded-xl z-10"></span>
-        <span className="absolute h-2 w-16 bg-gradient-to-t from-[#FCBB53] dark:from-[#FE9492] via-[#FB65A4] to-[#A67DD2] dark:to-[#0D0DA3] rounded-xl"></span>
+        <span className="absolute w-1 md:w-2 h-10 md:h-16 bg-gradient-to-t from-[#FCBB53] dark:from-[#FE9492] via-[#FB65A4]   to-[#A67DD2] dark:to-[#0D0DA3] rounded-xl z-10"></span>
+        <span className="absolute h-1 md:h-2 w-10 md:w-16 bg-gradient-to-t from-[#FCBB53] dark:from-[#FE9492] via-[#FB65A4] to-[#A67DD2] dark:to-[#0D0DA3] rounded-xl"></span>
       </button>
       <Link href="/">
         <Image
