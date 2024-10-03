5ca5c8cd8d8328f564fe7899c972989a04dcece1
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-10-03 21:51:12 +0300

    update

--- components/shared/ContactUs.tsx
+++ components/shared/ContactUs.tsx
@@ -7,14 +7,14 @@ const ContactUs = () => {
   return (
     <div className="max-w-[80vw] w-full h-full flex flex-col justify-center items-center shadow-xl bg-white dark:bg-black py-4 md:py-8 rounded-xl overflow-hidden">
       <div className="w-full flex flex-col items-center justify-center px-20 gap-4 md:gap-8 lg:gap-12 py-4 md:py-8 lg:py-12">
-        <h3 className="font-bold text-xl lg:text-3xl">Booking :</h3>
-        <h5 className="font-normal text-xl lg:text-3xl text-center">
-          Sunset Agency - Adélia Cailleux
+        <h3 className="font-bold text-xl lg:text-3xl uppercase">Booking :</h3>
+        <h5 className="font-normal text-xl lg:text-3xl text-center uppercase flex flex-col md:flex-row md:gap-4">
+          Sunset Agency <span>-</span> Adélia Cailleux
         </h5>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-2 lg:gap-4">
           <Link
             href="https://wa.me/+33603211791"
-            className="underline text-lg text-center"
+            className="underline text-lg text-center uppercase"
             target="_blank"
           >
             +33 6 03 2117 91
@@ -22,7 +22,7 @@ const ContactUs = () => {
           <span className="hidden lg:inline-block font-semibold">/</span>
           <Link
             href="mailto:adelia.booking@gmail.com?subject=Subject%20Here&body=Body%20Text%20Here"
-            className="underline text-lg text-center "
+            className="underline text-lg text-center uppercase "
           >
             adelia.booking@gmail.com
           </Link>
