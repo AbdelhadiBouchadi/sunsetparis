6adadb3891b9bba5b7f509a763c950d3f9619a7c
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-10-04 09:49:55 +0300

    artist pages content placeholders

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jost.variable} antialiased`}>
+      <body className={`${jost.variable} antialiased bg-background`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
