773a113323498ae34858eb5e2e2fa0b222a5e07a
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-10-14 18:51:22 +0300

    contact modal adjusting

--- components/shared/ContactUs.tsx
+++ components/shared/ContactUs.tsx
@@ -7,22 +7,23 @@ const ContactUs = () => {
   return (
     <div className="max-w-[80vw] w-full h-full flex flex-col justify-center items-center shadow-xl bg-white dark:bg-black py-4 md:py-8 rounded-xl overflow-hidden">
       <div className="w-full flex flex-col items-center justify-center px-20 gap-4 md:gap-8 lg:gap-12 py-4 md:py-8 lg:py-12">
-        <h3 className="font-bold text-xl lg:text-3xl uppercase">Booking :</h3>
-        <h5 className="font-normal text-xl lg:text-3xl text-center uppercase flex flex-col md:flex-row md:gap-4">
-          Sunset Agency <span>-</span> Adélia Cailleux
+        <h3 className="font-bold text-xl lg:text-3xl uppercase text-transparent bg-clip-text bg-gradient-to-t from-[#FCBB54] via-[#FB65A4] to-[#A67DD2]">
+          contact sunset
+        </h3>
+        <h5 className="font-normal text-xl lg:text-3xl text-center uppercase text-transparent bg-clip-text bg-gradient-to-t from-[#FCBB54] via-[#FB65A4] to-[#A67DD2] ">
+          Adélia Cailleux
         </h5>
-        <div className="flex flex-col lg:flex-row items-center justify-center gap-2 lg:gap-4">
+        <div className="flex flex-col  items-center justify-center gap-2 lg:gap-4">
           <Link
             href="https://wa.me/+33603211791"
-            className="underline text-lg text-center uppercase"
+            className=" text-lg text-center uppercase text-transparent bg-clip-text bg-gradient-to-t from-[#FCBB54] via-[#FB65A4] to-[#A67DD2]"
             target="_blank"
           >
             +33 6 03 2117 91
           </Link>
-          <span className="hidden lg:inline-block font-semibold">/</span>
           <Link
             href="mailto:adelia.booking@gmail.com?subject=Subject%20Here&body=Body%20Text%20Here"
-            className="underline text-lg text-center uppercase "
+            className=" text-lg text-center uppercase text-transparent bg-clip-text bg-gradient-to-t from-[#FCBB54] via-[#FB65A4] to-[#A67DD2] "
           >
             adelia.booking@gmail.com
           </Link>
