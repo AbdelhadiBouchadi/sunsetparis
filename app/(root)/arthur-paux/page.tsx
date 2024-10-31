7b3e59dc844e7f205c09a994e5c05bb9db14b40a
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-10-31 10:37:17 +0300

    db connection modification

--- app/(root)/arthur-paux/page.tsx
+++ app/(root)/arthur-paux/page.tsx
@@ -1,8 +1,10 @@
 import ProjectsGrid from '@/components/shared/ProjectsGrid';
 import { getArthurProjects } from '@/lib/actions/project.actions';
+import { ensureDatabaseConnection } from '@/lib/database';
 import React from 'react';
 
 const page = async () => {
+  await ensureDatabaseConnection();
   const projects = await getArthurProjects();
 
   return <ProjectsGrid projects={projects} />;
