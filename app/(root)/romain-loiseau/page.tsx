b81ddab831d42943a55abee1063949677380001f
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-12-08 22:10:08 +0300

    updating the server actions & projectForm

--- app/(root)/romain-loiseau/page.tsx
+++ app/(root)/romain-loiseau/page.tsx
@@ -2,6 +2,9 @@ import ProjectsGrid from '@/components/shared/ProjectsGrid';
 import { getRomainProjects } from '@/lib/actions/project.actions';
 import React from 'react';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const page = async () => {
   const projects = await getRomainProjects();
 
