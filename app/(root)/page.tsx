c623e58f62735acf38a0cb0a9ea8fb4b988a9b6b
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2025-02-04 21:08:33 +0300

    hiding artist names

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { artists } from '@/constants';
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 const page = () => {
   const [mounted, setMounted] = useState(false);
+  const visibleArtists = ['gabriel porier', 'romain loiseau'];
 
   useEffect(() => {
     setMounted(true);
@@ -20,7 +22,10 @@ const page = () => {
           <li key={index} className="w-full flex justify-center items-center">
             <Link
               href={artist.path}
-              className="uppercase text-3xl sm:text-4xl text-center font-light tracking-tighter hover:text-transparent bg-clip-text bg-gradient-to-t from-[#FCBB54] via-[#FB65A4] to-[#A67DD2] transition duration-300 px-2"
+              className={cn(
+                'uppercase text-3xl sm:text-4xl text-center font-light tracking-tighter hover:text-transparent bg-clip-text bg-gradient-to-t from-[#FCBB54] via-[#FB65A4] to-[#A67DD2] transition duration-300 px-2',
+                !visibleArtists.includes(artist.name) && 'hidden'
+              )}
             >
               {artist.name}
             </Link>
